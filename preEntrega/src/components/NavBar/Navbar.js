e5328d2e446de5c73a64839507ecb3e2f633710d
Rodrigo Medina Arrojo <marrojorodrigo@gmail.com>
2023-03-12 18:40:11 -0300

    NavBar

---
+++ preEntrega/src/components/NavBar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+const navBar = () => {
+    return (
+        <nav>
+            <p>Home</p>                           
+            <p>About</p>                           
+            <p>Services</p>                           
+            <p>Products</p>                           
+        </nav>
+    )
+}
+
+export default navBar
